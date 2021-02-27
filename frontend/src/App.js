7c1fac58b79fafb2a72068418881fab1b8992c15
17etro <petrpetryk2016@gmail.com>
2021-02-27 20:43:07 +0200

    rebuild auth page with react.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Auth from './pages/Auth/Auth';
+
+const App = () => {
+    return (
+      <div>
+        <Auth />
+      </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/Auth/Auth.js
@@ -0,0 +1,173 @@
+import React, { useState } from 'react';
+
+import './Auth.scss';
+
+//images 
+import loginSvg from '../../assets/images/Auth/login.svg';
+import registrSvg from '../../assets/images/Auth/register.svg';
+import eyeOpen from '../../assets/images/Auth/eye.svg';
+import eyeClosed from '../../assets/images/Auth/eyeClosed.svg';
+
+const Auth = (props) => {
+
+    const [ showPass, setShowPass ] = useState(false);
+
+    const [ logInData, setLogInData ] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [ signUpData, setSignUpData ] = useState({
+        email: '',
+        name: '',
+        surname: '',
+        password: '',
+        secondPassword: ''
+    });
+
+    const onChangeMode = (value) => {
+        if (value === 'signup') {
+            document.getElementById("register").style.left = "0px";
+	        document.getElementById("login").style.left = "-650px";
+        } else if (value === 'login') {
+            document.getElementById("register").style.left = "650px";
+	        document.getElementById("login").style.left = "0";
+        }
+    };
+    const onChangeInput = (event, category, key) => {
+        if (category === 'login') {
+            const newLogInData = {
+                ...logInData
+            };
+            newLogInData[key] = event.target.value;
+            setLogInData(newLogInData);
+        } else if (category === 'signup') {
+            const newSignUpData = {
+                ...signUpData
+            };
+            newSignUpData[key] = event.target.value;
+            setSignUpData(newSignUpData);
+        }
+    };
+
+    const onChangeShowPass = () => {
+        setShowPass(!showPass);
+    }
+
+    const onLogIn = () => {
+        // request
+    };
+
+    const onSignUp = () => {
+        // request
+    };
+
+    console.log(signUpData);
+
+    return (
+        <div className="auth">
+				<div className="auth_container">
+
+                    {/* LOGIN */}
+					<div id="login" className="auth_container_body">
+						<h1>Вітаємо вас</h1>
+						<img 
+                            src={loginSvg}
+                            alt="auth" 
+                        />
+						<div className="auth_container_topbar">
+							<div className="active-item">
+								<p>Вхід</p>
+							</div>
+							<div className="item" onClick={()=>onChangeMode('signup')}>
+								<p>Реєстрація</p>
+							</div>
+						</div>
+						<input 
+                            value={logInData.email}
+                            onChange={(e) => onChangeInput(e, 'login', 'email')}
+                            placeholder="Електронна пошта" 
+                            type="text" 
+                        />
+						<div className="input">
+							<input 
+                                value={logInData.password}
+                                onChange={(e) => onChangeInput(e, 'login', 'password')}
+                                placeholder="Пароль" 
+                                type={showPass ? "text" : "password"} 
+                            />
+                            <div className="eye" onClick={onChangeShowPass}>
+                                <img src={showPass ? eyeOpen : eyeClosed} alt="" />
+                            </div>
+						</div>
+						<button 
+                            onClick={onLogIn}
+                            className="submit"
+                        >
+                            Ввійти
+                        </button>
+					</div>
+
+					{/* REGISTER */}
+					<div id="register" className="auth_container_body">
+						<h1>Вітаємо вас</h1>
+						<img 
+                            src={registrSvg}
+                            alt="auth" 
+                        />
+						<div className="auth_container_topbar">
+							<div className="item" onClick={()=>onChangeMode('login')}>
+								<p>Вхід</p>
+							</div>
+							<div className="active-item">
+								<p>Реєстрація</p>
+							</div>
+						</div>
+						<input 
+                            value={signUpData.name}
+                            onChange={(e) => onChangeInput(e, 'signup', 'name')}
+                            placeholder="Ім'я" 
+                            type="text" 
+                        />
+						<input 
+                            value={signUpData.surname}
+                            onChange={(e) => onChangeInput(e, 'signup', 'surname')}
+                            placeholder="Прізвище" 
+                            type="text" 
+                        />
+						<input 
+                            value={signUpData.email}
+                            onChange={(e) => onChangeInput(e, 'signup', 'email')}
+                            placeholder="Електронна пошта" 
+                            type="text" 
+                        />
+						<div className="input">
+							<input 
+                                value={signUpData.password}
+                                onChange={(e) => onChangeInput(e, 'signup', 'password')}
+                                placeholder="Пароль" 
+                                type={showPass ? "text" : "password"} 
+                            />
+                            <div className="eye" onClick={onChangeShowPass}>
+                                <img src={showPass ? eyeOpen : eyeClosed} alt="" />
+                            </div>
+						</div>
+						<input 
+                            value={signUpData.secondPassword}
+                            onChange={(e) => onChangeInput(e, 'signup', 'secondPassword')}
+                            placeholder="Підтвердіть пароль" 
+                            type="password" 
+                        />
+						<button 
+                            onClick={onSignUp}
+                            className="submit"
+                        >
+                            Зареєструватися
+                        </button>
+					</div>
+				</div>
+			</div>
+    );
+};
+
+export default Auth;
