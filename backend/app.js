5c1d436aa447977be38bdc184f6a2db44639d86b
17etro <petrpetryk2016@gmail.com>
2021-03-07 15:20:32 +0200

    add simple server. connect with frontend

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,45 @@
 
 require('dotenv/config');
 const http = require('http');
+const { POSTrouter, PUTrouter, GETrouter, DELETErouter } = require('./routes/routes');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello world');
+
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
+    'Access-Control-Max-Age': 2592000,
+    "Access-Control-Allow-Headers": "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept",
+    "Access-Control-Allow-Credentials": false
+  };
+  res.writeHead(204, headers);
+
+  // methods
+  console.log(req.method);
+  switch (req.method) {
+    case "GET": {
+      console.log('get');
+      break;
+    }
+    case "POST": {
+      const url = req.url;
+      const func = POSTrouter[url];
+      func();
+      res.end('hello');
+      break;
+    }
+    case "DELETE": {
+      console.log('delete');
+      break;
+    }
+    case "PUT": {
+      console.log('put');
+      break;
+    }
+  };
+
 });
 
-server.listen(process.env.PORT);
-console.log(`server running in ${process.env.HOST}:${process.env.PORT}`);
+server.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(`server running in ${process.env.HOST}:${process.env.PORT}`);
+});
---
+++ backend/routes/routes.js
@@ -0,0 +1,23 @@
+const POSTrouter = {
+    '/login': () => console.log('log-in'),
+    '/signup': () => console.log('sign-up')
+};
+
+const GETrouter = {
+
+};
+
+const PUTrouter = {
+
+};
+
+const DELETErouter = {
+
+};
+
+module.exports = {
+    POSTrouter,
+    GETrouter,
+    PUTrouter,
+    DELETErouter
+};
