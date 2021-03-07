db63026413254909846804e34bbbeb55bce8f8c6
17etro <petrpetryk2016@gmail.com>
2021-03-07 13:18:18 +0200

    add env

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,11 @@
-const fastify = require('fastify')({
-    logger: true
-  })
-  
-  // Declare a route
-  fastify.get('/', function (request, reply) {
-    reply.render(__dirname + 'views/html/main.html')
-  })
-  
-  // Run the server!
-  fastify.listen(3000, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    fastify.log.info(`server listening on ${address}`)
-  })
+'use strict';
+
+require('dotenv/config');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello world');
+});
+
+server.listen(process.env.PORT);
+console.log(`server running in ${process.env.HOST}:${process.env.PORT}`);
