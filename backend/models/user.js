cde4a3480cb3816b052e22de9dd9b63cbf00a6af
Artem-Vladimirov-IP95 <mypochtav@gmail.com>
2021-03-11 14:15:30 +0200

    add delete method to user

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,12 +8,20 @@ class User {
             ...data
         };
     }
-    // static methods
+    //update user data
+    // update() {
+        
+    // }
+    //delete form database
+    delete() {
+        const { id } = this.info;
+        const command = 'DELETE FROM users WHERE id = $1';
+        const insertValues = [id];
 
-    // methods
-
+        return pool.query(command, insertValues);
+    }
     // save new user to database
-    saveToDB() {
+    save() {
         const { name, surname, email, password } = this.info;
         const command = 'INSERT INTO users( name, surname, email, password ) VALUES($1, $2, $3, $4)';
         const insertValues = [name, surname, email, password];
