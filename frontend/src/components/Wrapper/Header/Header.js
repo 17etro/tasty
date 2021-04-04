af561025aa0998dc6639f0cf37ca67e7a11940bc
GVBilokorenko <abilokorenko@gmail.com>
2021-04-04 15:19:16 +0300

    Modified/fixed user interface

--- frontend/src/components/Wrapper/Header/Header.js
+++ frontend/src/components/Wrapper/Header/Header.js
@@ -1,45 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import './header.scss';
+import "./header.scss";
 
-import logo from '../../../assets/images/Main/logo.png';
+import logo from "../../../assets/images/Main/logo.png";
 
-const Header = (props) => {
-    return (
-        <div className="navbar">
-            <div className="container flex">
-                <div className="flex nav" style={{minWidth: '410px'}} >
-                    <i className="fas fa-bars"></i>
-                    <div className="image">
-                        <img src={logo} alt="Tasty" />
-                    </div>
-                    <ul>
-                        <li>
-                            <div id="searchBar">
-                                <input type="text" id="search" />
-                                <i 
-                                    className="fas fa-search" 
-                                    style={{float: 'right', fontSize: '20px'}}
-                                />
-                            </div>
-                        </li>
-                        <li>
-                            <button>
-                                <i className="fas fa-plus"></i>
-                                Додати рецепт
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-                <div className="main-auth flex" style={{ minWidth: '210px' }} >
-                    <i className="fas fa-user"></i>
-                    <button className="hide">Увійти</button>
-                    <div className="divider hide"></div>
-                    <button className="hide">Зареєструватися</button>
-                </div>
-            </div>
-        </div>
-    );
+const Header = props => {
+	return (
+		<div className="navbar">
+			<div className="container flex">
+				<div className="flex">
+					<i className="fas fa-bars"></i>
+					<div className="image">
+						<img src={logo} alt="Tasty" />
+					</div>
+					<div id="searchBar">
+						<input type="text" id="search" />
+						<i className="fas fa-search" />
+					</div>
+					<button>
+						<i className="fas fa-plus"></i>
+						Додати рецепт
+					</button>
+				</div>
+				<div className="main-auth flex">
+					<i className="fas fa-user"></i>
+					<button className="hide">Увійти</button>
+					<div className="divider hide"></div>
+					<button className="hide">Зареєструватися</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -1,104 +1,119 @@
 import React from "react";
 
 // icons
-import recipe from '../../assets/images/Main/recipe.jpg';
+import recipe from "../../assets/images/Main/recipe.jpg";
 
 import "./main.scss";
 
-const Main = (props) => {
-    return (
-        <div className="main">
-            {/* Showcase */}
-            <section className="showcase">
-                <div className="recipesTable container">
-                    <div class="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                    <div className="recipe">
-                        <img className="image" src={recipe} alt="" />
-                        <div className="textField">
-                            <i className="far fa-user"></i>
-                            <p className="nick">Nick</p>
-                            <p className="description">Description</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <div className="flex" style={{
-                backgroundColor: '#eeeeee',
-                color: '#333'
-            }}>
-                <ul>
-                    <li><p>1</p></li>
-                    <li><p>2</p></li>
-                    <li><p>3</p></li>
-                    <li><p>4</p></li>
-                    <li><p>5</p></li>
-                </ul>
-            </div>
-        </div>
-    );
+const Main = props => {
+	return (
+		<div className="main">
+			{/* Showcase */}
+			<section className="showcase">
+				<div className="recipesTable">
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+					<div class="recipe">
+						<img className="image" src={recipe} alt="" />
+						<div className="field">
+							<div>
+								<i className="far fa-user"></i>
+							</div>
+							<p className="nick">Nick</p>
+							<p className="description">Description</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<footer>
+				<div className="flex">
+					<div className="page">1</div>
+					<div className="page">2</div>
+					<div className="page">3</div>
+					<div className="page">4</div>
+					<div className="page">5</div>
+				</div>
+			</footer>
+		</div>
+	);
 };
 
 export default Main;
